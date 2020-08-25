167612693c475ead214e2fca55164ead420d6bec
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-25 14:50:16 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Tom", last_name: "Jackson")
+Student.create(first_name: "Alex", last_name: "Foreman")
