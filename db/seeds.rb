9cd1d324ee0bd5543dfe79e9429e1a87e4b5362e
Mar5Planet <amustaf@email.arizona.edu>
2020-08-25 15:42:01 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do 
+Student.create(first_name: Faker::Name.unique.name, last_name: Faker::Name.unique.name)
+end
+
